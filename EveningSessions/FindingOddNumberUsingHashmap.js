aaff432118eecc55d6815af53542e8ea205d08b4
PemaDechen <padenlaa@gmail.com>
2025-09-25 13:54:36 +0530

    DSA avoided = interviews failed, confidence broken, regret later. You need to move from I understand to I can execute.

--- EveningSessions/FindingOddNumberUsingHashmap.js
+++ EveningSessions/FindingOddNumberUsingHashmap.js
@@ -3,7 +3,7 @@ const oddNumber = (arr) => {
   arr.map((data) => (hashObj[data] = hashObj[data] ? hashObj[data] + 1 : 1));
 
   for (const key in hashObj) {
-    if (hashObj[key] !== 3) {
+    if (obj[key] !== 3) {
       // To find a true 'odd' frequency
       return parseInt(key); // Convert the key (which is a string) back to a number
     }
@@ -11,3 +11,4 @@ const oddNumber = (arr) => {
 };
 
 console.log(oddNumber([1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]))
+// DSA avoided = interviews failed, confidence broken, regret later. You need to move from "I understand" to "I can execute.
